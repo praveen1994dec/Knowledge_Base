c6d48a8d353e504b53399991eee8dcae109d387c
praveen singampalli <praveensingampalli1994@gmail.com>
2026-01-14 20:35:35 +0530

    changes done

--- Shell_Scripting/DevOpsBatch14.sh
+++ Shell_Scripting/DevOpsBatch14.sh
@@ -3,6 +3,7 @@
 API_URL="https://api.example.com/pharmacy/delivery"
 API_KEY="YOUR_API_KEY"
 
+
 curl -s "$API_URL" \
   -H "Authorization: Bearer $API_KEY" \
   -H "Accept: application/json" \
@@ -10,3 +11,19 @@ curl -s "$API_URL" \
 
 
 ###Hi this is praveen here
+
+Devsopbarnch
+Step - I have changed the file/ project in devops branch and someone 
+has already changed the main branch code which i have not pulled
+and then i tried to merge to parent main branch THEN CONFLICH OCCURS
+
+
+WHY CONFLICT???
+
+Same file is changed by multiple people and the 
+changed data is not in my foldfer or repo
+###Changing the data
+
+>>>>>>>>66669wdihodxiaxpaxln<><<<<<
+<<<<<Merge conlfict <>>>>>>>
+>>>>>>> d21626c (changhes done)
